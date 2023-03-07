5a72f8b2709a1febc6005601951b9855b466c426
GkantirisRafael <gkantirisrafael@gmail.com>
2023-03-07 16:48:49 +0200

    Final Assignment commit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('Modified_Table.csv')
+
+df1 = df.plot(x='zip_code' , y=['bottles_sold'],linestyle=":",marker='o')
+plt.xlabel('Zip Code')
+plt.ylabel('Bottles Sold')
+plt.title('Bottles Sold per Zip Code')
+plt.show()
